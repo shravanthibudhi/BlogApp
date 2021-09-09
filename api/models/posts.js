300acffaeeea31429a8a54e30638aa8c28e2e694
Shravanthi Budhi <66373469+shravanthibudhi@users.noreply.github.com>
2021-09-09 15:37:52 -0400

    Add files via upload

--- api/models/posts.js
+++ api/models/posts.js
@@ -3,18 +3,39 @@ const PATH = "./data.json";
 
 class Posts {
 
-    add(newPost) {
-        const data = this.readData();
+    get() {
+        /** Get Posts */
+        return this.readData();
     }
 
-    get() {
-        return this.readData();
+    getIndividualBlog(postId) {
+        /** Get one blog post */
+        const posts = this.readData();
+        const foundPost = posts.find((post) => post.id == postId);
+        return foundPost;
+    }
+
+    add(newPost) {
+        /** Add new Post */
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        // unshift appends the array and shifts an object to the top 
+        // then we can store the data
+        this.storeData(currentPosts);
     }
 
     readData() {
         let rawData = fs.readFileSync(PATH);
-        let posts = JSON.parse();
+        let posts = JSON.parse(rawData);
+        return posts;
+    }
+
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 }
+
+    
 
 module.exports = Posts;
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const Post = require('../blog-api/api/models/posts');
@@ -6,5 +7,16 @@ const postsData = new Post();
 app.get("/api/posts", (req, res)=>{
     res.status(200).send(postsData.get());
 })
+
+app.get("/api/posts/:post_id", (req, res)=>{
+    const postId = req.params.post_id;
+    const foundPost = postsData.getIndividualBlog(postId);
+    if(foundPost) {
+        res.status(200).send(foundPost)
+    } else {
+        res.status(404).send("Not Found");
+    }
+})
+
 
 app.listen(8080, ()=>console.log("Listening on http://localhost:8080/"));
